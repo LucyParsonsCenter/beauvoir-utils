e199ee53e8edbc7a8b0c4b50b00a579b95410066
Alice Pote <alice.writes.wrongs@gmail.com>
2015-09-07 12:24:04 -0400

    wip

--- upgrade_inshopkeeper.sh
+++ upgrade_inshopkeeper.sh
@@ -14,7 +14,7 @@ fi
 
 echo "configuring infoshopkeeper database"
 echo "create user 'isk@localhost' identified by 'isk'" | mysql -u root -p
-echo "create database idk" | mysql -u root -p
+echo "create database isk" | mysql -u root -p
 echo "grant all privileges on isk.* to isk identified by 'isk'" | mysql -u root -p
 
 git checkout 8a33fbe

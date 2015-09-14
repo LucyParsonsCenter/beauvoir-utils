bbf2cd861d4b1dcdf2c97b169a43d5079637c2e1
Alice Pote <alice.writes.wrongs@gmail.com>
2015-09-14 19:16:58 -0400

    more better!

--- upgrade_inshopkeeper.sh
+++ upgrade_inshopkeeper.sh
@@ -15,10 +15,12 @@ else
 fi
 
 echo "configuring infoshopkeeper database"
+echo "type in the mysql root password 3x"
 echo "create user 'isk@localhost' identified by 'isk'" | mysql -u root -p
 echo "create database isk" | mysql -u root -p
 echo "grant all privileges on isk.* to isk identified by 'isk'" | mysql -u root -p
 echo "importing infoshop db dump"
+echo "type in 'isk' for this one!"
 mysql -u isk -p -h localhost isk < $1
 
 bundle exec rake infoshopkeeper:import
